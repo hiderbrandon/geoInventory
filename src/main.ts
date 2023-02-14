f3dac07dbcc82db48b231b4d0db2c68e2326acb8
hider brandon vargas <hiderbrandon@gmail.com>
2023-02-14 13:58:01 -0500

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,13 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule , { cors: true });
+  const app = await NestFactory.create(AppModule );
+  app.enableCors({
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+  });
 
   const config = new DocumentBuilder()
   .setTitle('inventory example')
