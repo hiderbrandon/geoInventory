233b20ae2c799bfd06adeca5eeee772415a7f185
hider brandon vargas <hiderbrandon@gmail.com>
2023-02-14 14:17:59 -0500

    cors improve erase error line

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-import CorsMidd
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule );
+
   app.enableCors({
     origin: "*",
     methods: ["GET","HEAD","PUT","PATCH","POST","DELETE"],
     preflightContinue: false,
     optionsSuccessStatus: 204
   });
-
+  
   const config = new DocumentBuilder()
   .setTitle('inventory example')
   .setDescription('API for manage an visualization')
