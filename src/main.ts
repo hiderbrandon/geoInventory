f5cd729b5dc09e5770d499828603aa037dd643af
hider brandon vargas <hiderbrandon@gmail.com>
2023-02-14 14:29:53 -0500

    cors strategy changed

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,12 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule );
-
-  app.enableCors({
-    origin: "*",
-    methods: ["GET","HEAD","PUT","PATCH","POST","DELETE"],
-    preflightContinue: false,
-    optionsSuccessStatus: 204
+  const app = await NestFactory.create(AppModule , {
+    cors: {
+      origin: '*',
+    },
   });
-  
+
   const config = new DocumentBuilder()
   .setTitle('inventory example')
   .setDescription('API for manage an visualization')
