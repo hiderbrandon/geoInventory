407b8f6812ce28a1b39558866d207fc1ff010ded
hider brandon vargas <hiderbrandon@gmail.com>
2023-02-14 14:14:43 -0500

    cors improve

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
+import CorsMidd
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule );
   app.enableCors({
-    "origin": "*",
-    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-    "preflightContinue": false,
-    "optionsSuccessStatus": 204
+    origin: "*",
+    methods: ["GET","HEAD","PUT","PATCH","POST","DELETE"],
+    preflightContinue: false,
+    optionsSuccessStatus: 204
   });
 
   const config = new DocumentBuilder()
